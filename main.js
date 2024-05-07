1e0b62927ebbddc25002194ced74125b71d7745c
Larsieeeee <larszwerver@gmail.com>
2024-05-07 14:17:24 +0200

    rekenmachine klaar

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-var nummer = document.getElementById("rekenmachine");
-
-function number(){
-   
+function number(value){
+    document.getElementById('kaart').value += value;
 }
 
+function del(){
+    document.getElementById('kaart').value = '';
+}
 
+function cal(){
+    try{
+        var nummer = eval(document.getElementById('kaart').value);
+        document.getElementById('kaart').value = nummer;
+    } catch(error){
+        document.getElementById('kaart').value = 'Error'
+    }
+}
